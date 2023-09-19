5aa84b24a83d5fa5b84632a50fb50903817d36ca
Jin <jin41879@gmail.com>
2023-09-19 17:15:59 +0900

    1

---
+++ 230919/demo_If.py
@@ -0,0 +1,17 @@
+# score = int(input("Enter your Score : "))
+# if score >= 60 :
+#     print("Success")
+# else :
+#     print("fall")
+
+season = input("Which season do you like?").lower()
+if season == "spring" :
+    print("AAA")
+elif season == "summer" :
+    print("BBB")
+elif season == "autumn" :
+    print("FFF")
+elif season == "winter" :
+    print("winter is coming")
+else :
+    print("Write the season you know!") 
