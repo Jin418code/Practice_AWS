15a1c66e4d148286173ebe261b1f5aaeda449ca5
Jin <jin41879@gmail.com>
2023-09-19 16:43:33 +0900

    first

---
+++ 230919/Lab_111.py
@@ -0,0 +1,44 @@
+# myString = "This is a String"
+# print(myString)
+
+# print(type(myString))
+
+
+
+# firstString = "water"
+# secondString = "fall"
+# thirdString = firstString + secondString
+# print(thirdString)
+
+
+
+# name = input("What is your name?")
+# color = input("What is your favorite color?")
+# animal = input("What is your favorite animal")
+
+# print("{}, you like a {} {}!".format(name,color,animal))
+
+
+
+# name = "Jin"
+# age = 29
+# print("Name is " + name + ", Age is " + str(age))
+# print("Name is %s, Age is %d" % (name, age))
+# print(f"Name is {name}, Age is {age}.")
+# print(f"Name is {name:10s}, Age is {age:5d}.") #s는 왼쪽정렬/d는 오른쪽정렬
+
+
+
+# student = {}
+# student["name"] = "Michael"
+# student["age"] = 24
+# student["address"] = "서울시 강남구 역삼동"
+
+# print(student)
+# print(f"{student['address']}")
+
+
+
+tuple_ = "banana", 50, 90.5, True
+# tuple_[3] = False  # 튜플은 수정 안됨
+print(tuple_ * 3)
